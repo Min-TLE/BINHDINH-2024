23d1b62dab220953e52eea4888459ecf68051d5f
NguyenTheAn <166810401+Min-TLE@users.noreply.github.com>
2024-10-24 08:10:34 +0700

    cac

--- choose.cpp
+++ choose.cpp
@@ -16,29 +16,25 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
+    ll k = a.size();
+    ll ans = 0;
+    for(int i = 0; i < n; i++){
+        vector<ll> b;
+        b.insert(b.end(), a.end() - i, a.end());
+        b.insert(b.end(), a.begin(), a.end() - i);
 
-    ll cnt1 = 1, cnt2 = 1;
-    ll m1 = a[0];
-    vector<ll> b;
-    auto t = min_element(a.begin(), a.end());
-    ll d = distance(a.begin(), t); 
-    for(int i = d; i < n; i++) b.push_back(a[i]);
-    for(int i = 0; i < d; i++) b.push_back(a[i]); 
-
-    ll m2 = b[0];
-    for(int i = 1; i < n; i++){
-        if(a[i] > m1){
-            m1 = a[i];
-            cnt1++;
+        ll m = -1;
+        ll cr_rmv = 0;
+        for(auto it : b){
+            if(it > m){
+                cr_rmv++;
+                m = it;
+                
+            }
         }
-        if(b[i] > m2){
-            m2 = b[i];
-            cnt2++;
-        }
+        ans = max(ans,cr_rmv);
     }
+    cout << ans;
     
-
-    cout << max(cnt1, cnt2) << endline;
-
     return 0;
 }
